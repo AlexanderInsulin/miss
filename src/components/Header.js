cee9ecfcb5be1df35e30cea1d5d5409f1a57c6a2
Alex Insulin <insulin@protonmail.com>
2018-04-12 15:32:04 +0300

    Add menu image to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Header.css';
+import menu from '../assets/menu.svg';
 
 const Header = (props) => (
   <div className="container">
+    <img src={menu}></img>
   </div>
 );
 
