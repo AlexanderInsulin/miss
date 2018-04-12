a0e4c63e3b6faa92be9db3419bbefb56fa959cde
Alex Insulin <insulin@protonmail.com>
2018-04-12 16:20:25 +0300

    Set default page to favorites.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import FavoritesPage from "./pages/FavoritesPage";
 
 const App = () => (
   <Router>
-    <Header />
+    <div>
+      <Header />
+
+      <Route exact path="/" component={FavoritesPage}/>
+    </div>
   </Router>
 );
 
--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 const FavoritesPage = () => (
-  
+  <div>
+  </div>
 )
+
+export default FavoritesPage;
