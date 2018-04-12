2c199dcceeabe457c30d872c70ac2f82e4f28be7
Alex Insulin <insulin@protonmail.com>
2018-04-12 15:37:56 +0300

    Style avatar and add dropdow button.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 import menu from '../assets/menu.svg';
 import logo from '../assets/logo.svg';
+import dropdown from '../assets/dropdown.svg';
 
 const Header = (props) => (
   <div className="container">
-    <img src={menu}></img>
+    <img src={menu} className="menu"></img>
     <img src={logo}></img>
-    <img src={menu}></img>
+    <div>
+      <img src={menu} className="avatar"></img>
+      <img src={dropdown} className="dropdown"></img>
+    </div>
   </div>
 );
 
