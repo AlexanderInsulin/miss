602ade909618293e9bf52a6f244e9da408fc8b38
Alex Insulin <insulin@protonmail.com>
2018-04-12 17:36:34 +0300

    Create footer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import FavoritesPage from "./pages/FavoritesPage";
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
       <Header />
 
       <Route exact path="/" component={FavoritesPage}/>
+      <Footer />
     </div>
   </Router>
 );
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer-container">
+    <a className="footer-link">Пользовательское соглашение</a>
+    <a className="footer-link">Справка и помощь</a>
+    <a className="footer-link">О сервисе</a>
+    <a className="footer-link">Обратная связь</a>
+    <a className="footer-copyrights">2018</a>
+    <a className="footer-copyrights">SkatTrading</a>
+  </div>
+);
+
+export default Footer;
