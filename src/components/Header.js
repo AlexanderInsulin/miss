04788162ee43cfb66ba326606f840c9283a72619
Alex Insulin <insulin@protonmail.com>
2018-04-12 15:32:13 +0300

    Add logo image to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Header.css';
 import menu from '../assets/menu.svg';
+import logo from '../assets/logo.svg';
 
 const Header = (props) => (
   <div className="container">
+    <img src={menu}></img>
+    <img src={logo}></img>
     <img src={menu}></img>
   </div>
 );
