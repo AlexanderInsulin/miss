42eeb338f94f6b0efe172972ac92a44cf9612ca8
Alex Insulin <insulin@protonmail.com>
2018-04-12 16:00:43 +0300

    Add react router dom.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
 
 const App = () => (
-  <Header></Header>
-)
+  <Router>
+    <Header />
+  </Router>
+);
 
 export default App;
