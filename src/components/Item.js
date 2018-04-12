850e5f5cecc180153d2cd1ca44cbbb7400d711b9
Alex Insulin <insulin@protonmail.com>
2018-04-12 17:19:46 +0300

    Make from item maket component.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import './Item.css'
-import favorite from '../assets/favorite.svg';
+import React from "react";
+import "./Item.css";
 
-const Item = () => (
+const Item = ({ name, img, coast, date }) => (
   <div>
     <div className="card-container">
       <div className="card-header">
-        <div className="card-header-tag">
-          Бытовая техника
-        </div>
+        <div className="card-header-tag">{name}</div>
         <div className="card-header-fav">
-          <img src={favorite}></img>
+          <img src={img} />
         </div>
       </div>
-      <div className="card-item-name">Iphone 7 S </div>
+      <div className="card-item-name">{name}</div>
     </div>
 
-    <div className="item-coast">
-      0,006 BTC
-    </div>
-    <div className="item-date">
-      06.04.2018
-    </div>
+    <div className="item-coast">{coast} BTC</div>
+    <div className="item-date">{date}</div>
   </div>
-)
+);
 
 export default Item;
--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Item from '../components/Item';
+import favorite from "../assets/favorite.svg";
 
 const FavoritesPage = () => (
   <div>
     <PageTitle text="Избранное" count={4} />
-    <Item />
-    <Item />
-    <Item />
-    <Item />
+    <Item name="Iphone 7 S" img={favorite} coast="0,006" date="06.04.2018"/>
   </div>
 )
 
